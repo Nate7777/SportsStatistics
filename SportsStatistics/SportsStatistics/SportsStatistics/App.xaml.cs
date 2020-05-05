67bfd59b194cc1571ed02a0743c4ccdbaccb2622
Naco777 <naco2544@gmail.com>
2020-05-05 14:51:48 -0300

    Added WelcomePage as root navigation page in App.xaml.cs

--- SportsStatistics/SportsStatistics/SportsStatistics/App.xaml.cs
+++ SportsStatistics/SportsStatistics/SportsStatistics/App.xaml.cs
@@ -10,7 +10,7 @@ namespace SportsStatistics
         {
             InitializeComponent();
 
-            MainPage = new WelcomePage();
+            MainPage = new NavigationPage(new WelcomePage());
         }
 
         protected override void OnStart()
